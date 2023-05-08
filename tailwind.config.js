1fb0d1e4c02afc3530efb681150d9a544329d13c
Azizi <azizishaharuddin@gmail.com>
2023-05-08 22:38:13 +0800

    setup theme for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'h1': ['24px', {
+          lineHeight: `${24 * 1.5}px`,
+          fontWeight: '500',
+        }],
+        base: ['16px', {
+          lineHeight: `${16 * 1.5}px`,
+          fontWeight: '400',
+        }]
+      },
+      colors: {
+        kira: {
+          primary: '#22ABDB', //light blue
+          white: '#FFFFFF',
+          background: '#F7F9FA',
+          grayLine: '#E5E5E5', // light gray for line
+          grayText: '#8D8D8D', //darker gray for text
+          darkText: '#19252E',
+        }
+      }
+    },
   },
   plugins: [],
 }
